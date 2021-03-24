698723b53e92e9ced4b82ee9e5165e8674df8fb4
Emad A. Ismael <EmadAIsmael@yahoo.com>
2021-03-24 22:24:41 +0200

    Wed., Mar. 24th 2021 22:24 finished stage 3/4.

--- Number Base Converter/task/src/converter/Main.kt
+++ Number Base Converter/task/src/converter/Main.kt
@@ -2,6 +2,7 @@ package converter
 
 import java.math.BigInteger
 import kotlin.math.pow
+import kotlin.system.exitProcess
 
 fun BigInteger.decimalToBaseX(base: Int): String {
     /**
@@ -19,56 +20,52 @@ fun BigInteger.decimalToBaseX(base: Int): String {
 }
 
 fun basXToDecimal(v: String, base: Int): BigInteger {
+    /**
+     * convert from base x to base 10.
+     */
     val digits = "0123456789" + ('a'..'z').joinToString("")
     var value = BigInteger.ZERO
     for ((i, c) in v.reversed().withIndex()) {
-        val temp = (digits.indexOf(c) * base.toDouble().pow(i)).toString()
-        value += BigInteger(temp)
+        val temp = (digits.indexOf(c) * base.toDouble().pow(i)).toLong()
+        value += BigInteger(temp.toString())
     }
     return value
 }
 
-fun readCommand(): Int {
-    val commands = arrayOf(
-        "/from",
-        "/to",
-        "/exit"
-    )
-    var command = ""
-    while (command !in commands) {
-        print("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ")
-        command = readLine()!!
+fun readBases(): Pair<Int, Int> {
+    var baseFrom = 0
+    var baseTo = 0
+    var input: String
+    while (baseFrom !in 2..36 || baseTo !in 2..36) {
+        print("Enter two numbers in format: {source base} {target base} (To quit type /exit) ")
+        input = readLine()!!
+        if (input == "/exit")
+            exitProcess(0)
+        val (f, t) = input.split(" ").map { it.toInt() }
+        baseFrom = f
+        baseTo = t
     }
-    return commands.indexOf(command)
+    return Pair(baseFrom, baseTo)
 }
 
-fun convertToDecimal() {
-    print("Enter source number: ")
-    val n = readLine()!!
+fun readNumber(from: Int, to: Int): String {
 
-    print("Enter source base: ")
-    val b = readLine()!!.toInt()
+    print("Enter number in base $from to convert " +
+                "to base $to (To go back type /back) ")
 
-    println("Conversion to decimal result: ${basXToDecimal(n, b)}")
-}
-
-fun convertFromDecimal() {
-    print("Enter number in decimal system: ")
-    val n = readLine()!!.toBigInteger()
-
-    print("Enter target base: ")
-    val b = readLine()!!.toInt()
-
-    println("Conversion result: ${n.decimalToBaseX(b)}")
+    return readLine()!!
 }
 
 fun main() {
-
     while (true) {
-        when (readCommand()) {
-            0 -> convertFromDecimal()
-            1 -> convertToDecimal()
-            2 -> break
+        val (from, to) = readBases()
+
+        ReadNumber@while (true) {
+            val n = readNumber(from, to)
+            if (n == "/back") break@ReadNumber
+            val decimal = basXToDecimal(n, from)
+            val converted = decimal.decimalToBaseX(to)
+            println("Conversion result: $converted")
         }
     }
 }
